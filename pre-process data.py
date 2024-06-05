a011040b8d124680c473fbf5971c513f71a6cf16
Sugin Elankavi Rajendran <57343300+Sugin-Elankavi-Rajendran@users.noreply.github.com>
2024-06-05 17:25:36 +0530

    finding unique values in the data

--- pre-process data.py
+++ pre-process data.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
 df = pd.read_excel("Copper_Set.xlsx")
-df.head(2)
+#print(df.head(2))
+
+# print(len(df['item_date'].unique())) 
+# print(len(df['customer'].unique())) 
+# print(len(df['material_ref'].unique()))
+# print(len(df['product_ref'].unique())) 
+# print(len(df['delivery date'].unique())) 
