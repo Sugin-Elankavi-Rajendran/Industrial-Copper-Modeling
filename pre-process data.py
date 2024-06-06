ea45c53c0a5eb99e21b4ec9a8deb9306b91590ea
Sugin Elankavi Rajendran <suginelankavir@gmail.com>
2024-06-06 13:17:11 +0530

    Heatmap done

--- pre-process data.py
+++ pre-process data.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 df = pd.read_excel("Copper_Set.xlsx")
-#print(df.head(2))
+# print(df.head(2))
 
 # print(len(df['item_date'].unique())) 
 # print(len(df['customer'].unique())) 
@@ -14,8 +14,6 @@ df = pd.read_excel("Copper_Set.xlsx")
 
 missing_values = df.isnull().sum()
 # print(missing_values)
-
-# print(df.info())
 
 df['item_date'] = pd.to_datetime(df['item_date'], format='%Y%m%d', errors='coerce').dt.date
 df['quantity tons'] = pd.to_numeric(df['quantity tons'], errors='coerce', downcast='float')
@@ -31,63 +29,31 @@ df['selling_price'] = pd.to_numeric(df['selling_price'], errors='coerce', downca
 
 missing_values_count = df.isnull().sum()
 # print(missing_values_count)
-# print(df.shape)
-# df.info()
 
 df['material_ref'] = df['material_ref'].fillna('unknown')
-df = df.dropna()
+df.dropna(inplace=True)
 
 missing_values_count = df.isnull().sum()
 # print(missing_values_count)
-# print(df.shape)
 
-# df_sample = df.sample(frac=0.2, random_state=42)
+df_sample = df.sample(frac=0.2, random_state=42)
 
-# sns.histplot(df_sample['quantity tons'], kde=True)
-# plt.show()
-# sns.histplot(df_sample['country'], kde=True)
-# plt.show()
-# sns.histplot(df_sample['application'], kde=True)
-# plt.show()
-# sns.histplot(df_sample['thickness'], kde=True)
-# plt.show()
-# sns.histplot(df_sample['width'], kde=True)
-# plt.show()
-# sns.histplot(df_sample['selling_price'], kde=True)
-# plt.show()
+df_sample['selling_price'] = df_sample['selling_price'].apply(lambda x: np.nan if x <= 0 else x)
+df_sample['quantity tons'] = df_sample['quantity tons'].apply(lambda x: np.nan if x <= 0 else x)
+df_sample['thickness'] = df_sample['thickness'].apply(lambda x: np.nan if x <= 0 else x)
 
-mask1 = df['selling_price'] <= 0
-# print(mask1.sum())
-df.loc[mask1, 'selling_price'] = np.nan
+df_sample['selling_price_log'] = np.log(df_sample['selling_price'])
+df_sample['quantity tons_log'] = np.log(df_sample['quantity tons'])
+df_sample['thickness_log'] = np.log(df_sample['thickness'])
 
-mask1 = df['quantity tons'] <= 0
-# print(mask1.sum())
-df.loc[mask1, 'quantity tons'] = np.nan
-
-mask1 = df['thickness'] <= 0
-# print(mask1.sum())
-
-# df.isnull().sum()
-
-df.dropna(inplace=True)
-# print(len(df))
-
-# df_sample = df.sample(frac=0.2, random_state=42)
-
-# df_sample['selling_price_log'] = np.log(df_sample['selling_price'])
 # sns.histplot(df_sample['selling_price_log'], kde=True)
 # plt.show()
-
-# df_sample['quantity tons_log'] = np.log(df_sample['quantity tons'])
 # sns.histplot(df_sample['quantity tons_log'], kde=True)
 # plt.show()
-
-# df_sample['thickness_log'] = np.log(df_sample['thickness'])
 # sns.histplot(df_sample['thickness_log'], kde=True)
 # plt.show()
 
-# print(df.head())
+x = df_sample[['quantity tons_log', 'application', 'thickness_log', 'width', 'selling_price_log', 'country', 'customer', 'product_ref']].corr()
 
-x=df[['quantity tons_log','application','thickness_log','width','selling_price_log','country','customer','product_ref']].corr()
-
-sns.heatmap(x, annot=True, cmap="YlGnBu")
+# sns.heatmap(x, annot=True, cmap='viridis', center=0)
+# plt.show()
