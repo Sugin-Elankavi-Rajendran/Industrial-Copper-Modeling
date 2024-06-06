be19e074f7771d77f81752e79aa0baced2388c10
Sugin Elankavi Rajendran <suginelankavir@gmail.com>
2024-06-06 12:37:06 +0530

    Cleared skewness in the data

--- pre-process data.py
+++ pre-process data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_excel("Copper_Set.xlsx")
 #print(df.head(2))
@@ -40,17 +41,47 @@ missing_values_count = df.isnull().sum()
 # print(missing_values_count)
 # print(df.shape)
 
+# df_sample = df.sample(frac=0.2, random_state=42)
+
+# sns.histplot(df_sample['quantity tons'], kde=True)
+# plt.show()
+# sns.histplot(df_sample['country'], kde=True)
+# plt.show()
+# sns.histplot(df_sample['application'], kde=True)
+# plt.show()
+# sns.histplot(df_sample['thickness'], kde=True)
+# plt.show()
+# sns.histplot(df_sample['width'], kde=True)
+# plt.show()
+# sns.histplot(df_sample['selling_price'], kde=True)
+# plt.show()
+
+mask1 = df['selling_price'] <= 0
+# print(mask1.sum())
+df.loc[mask1, 'selling_price'] = np.nan
+
+mask1 = df['quantity tons'] <= 0
+# print(mask1.sum())
+df.loc[mask1, 'quantity tons'] = np.nan
+
+mask1 = df['thickness'] <= 0
+# print(mask1.sum())
+
+# df.isnull().sum()
+
+df.dropna(inplace=True)
+# print(len(df))
+
 df_sample = df.sample(frac=0.2, random_state=42)
 
-sns.histplot(df_sample['quantity tons'], kde=True)
+df_sample['selling_price_log'] = np.log(df_sample['selling_price'])
+sns.histplot(df_sample['selling_price_log'], kde=True)
 plt.show()
-sns.histplot(df_sample['country'], kde=True)
+
+df_sample['quantity tons_log'] = np.log(df_sample['quantity tons'])
+sns.histplot(df_sample['quantity tons_log'], kde=True)
 plt.show()
-sns.histplot(df_sample['application'], kde=True)
-plt.show()
-sns.histplot(df_sample['thickness'], kde=True)
-plt.show()
-sns.histplot(df_sample['width'], kde=True)
-plt.show()
-sns.histplot(df_sample['selling_price'], kde=True)
+
+df_sample['thickness_log'] = np.log(df_sample['thickness'])
+sns.histplot(df_sample['thickness_log'], kde=True)
 plt.show()
